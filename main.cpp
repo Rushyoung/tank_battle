cf76072d7924f1e80f6fd1aaaab977bc27fafc56
Wu Junkai <wu.junkai@qq.com>
2024-04-29 21:09:57 +0800

    chore: 引入 asio 库

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "asio.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    // curl from localhost:8080
+    asio::io_context io_context;
+    asio::ip::tcp::acceptor acceptor(io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 8080));
+    asio::ip::tcp::socket socket(io_context);
+    acceptor.accept(socket);
+    asio::streambuf buffer;
+    asio::read_until(socket, buffer, "\r\n");
+    std::istream stream(&buffer);
+    std::string request;
+    std::getline(stream, request);
+    std::cout << request << std::endl;
 }
