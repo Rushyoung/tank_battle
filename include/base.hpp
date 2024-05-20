c8b0274802e4ae8dd60a87a13ca1df6bc4cd9df6
Rushyoung <74442033+Rushyoung@users.noreply.github.com>
2024-05-20 16:45:18 +0800

    wip:redesign class

--- include/base.hpp
+++ include/base.hpp
@@ -1,7 +1,8 @@
 //
 // Created by 小小喵姬 on 24-5-13.
 //
-
+#define MAP_X
+#define MAP_Y
 #ifndef TANK_BATTLE_BASE_HPP
 #define TANK_BATTLE_BASE_HPP
 #define PI 3.14159265359
@@ -9,7 +10,7 @@
 
 class baseTank{
 public:
-    virtual void move();
+    virtual void move(double _degree = 0);
     int getX(){return x;}
     int getY(){return y;}
     double getDegree(){return degree;}
@@ -21,17 +22,17 @@ private:
 
 class Tank_local : public baseTank{
 public:
-    void move() override;
+    void move(double _degree = 0) override;
 };
 
 class Tank_ai : public baseTank{
 public:
-    void move() override;
+    void move(double _degree = 0) override;
 };
 
 class Tank_remote : public baseTank{
 public:
-    void move() override;
+    void move(double _degree = 0) override;
 };
 
 
